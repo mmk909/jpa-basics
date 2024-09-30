c72abd8814a9223a007a96acb19e038f8c79d3dc
Michael Lee <dev@mmk.com>
2024-09-30 20:47:48 +0800

    finish basic minimal checking

--- src/test/java/com/example/crud/CrudApplicationTests.java
+++ src/test/java/com/example/crud/CrudApplicationTests.java
@@ -1,14 +1,6 @@
 package com.example.crud;
 
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 @SpringBootTest
